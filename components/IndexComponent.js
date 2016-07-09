cf8616aa0b0c1d38be09e555df5998354bb89182
edawg11 <eliot.smith1185@gmail.com>
2016-07-09 14:44:05 -0400

    react-routing on backend

---
+++ components/IndexComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class IndexComponent extends React.Component {
+	render(){
+		return (
+			<div>
+				<p>this is the index page</p>
+			</div>
+		);
+	}
+}
--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 import express from 'express';
 import http from 'http';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { match , RouterContext } from 'react-router';
+import AppComponent from './components/AppComponent';
+import IndexComponent from './components/IndexComponent';
+
+const routes = {
+	path: '',
+	component: AppComponent,
+	childRoutes: [
+		{
+			path: '/',
+			component: IndexComponent
+		}
+	]
+}
+
+
+
 
 const app = express();
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('*', (req, res) => {
-    res.render('index');
+    match({ routes, location: req.url }, (err, redirectLocation, props) => {
+    	if(props) {
+    		const markup = renderToString(<RouterContext { ...props } />)
+    		console.log(markup);
+    		res.render('index', { markup })
+    	}
+    })
 });
 
 const server = http.createServer(app);
